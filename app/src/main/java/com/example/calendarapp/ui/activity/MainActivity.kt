91a3902cd00317fe7cf53f26e27542572f75dd9d
shkim908 <rlathgus1357@gmail.com>
2025-02-19 09:19:20 +0900

    sunday, saturday 색상 변경 수정

--- app/src/main/java/com/example/calendarapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/ui/activity/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.calendarapp.ui.activity
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.calendarapp.R
+import com.example.calendarapp.ui.material.CalendarDecorators
+import com.google.android.material.datepicker.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        initView()
+
     }
+
+    private fun initView(){
+        val calendarView: MaterialCalendarView = findViewById(R.id.calendar_view)
+
+        calendarView.addDecorators(CalendarDecorators.saturdayDecorator(applicationContext))
+        calendarView.addDecorators(CalendarDecorators.sundayDecorator(applicationContext))
+
+    }
+
 }
